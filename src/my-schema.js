03779bc635013e65262d51b5207f2c7ecec99f22
Marcus Deh <deh.marcus@outlook.de>
2020-09-17 00:41:13 +0200

    second sub

--- src/my-schema.js
+++ src/my-schema.js
@@ -3,8 +3,10 @@ const { gql } = require("apollo-server-express");
 module.exports = gql`
   type Query {
     go: String!
+    doSomething: String!
   }
   type Subscription {
+    stat: String!
     info: String!
   }
 `;
--- src/resolver.js
+++ src/resolver.js
@@ -1,29 +1,44 @@
 const { PubSub } = require("apollo-server-express");
 const pubsub = new PubSub();
 
-const TOPIC = "infoTopic";
+const TOPIC = "inTopic";
+const SECOND_TOPIC = "state";
 
 const infos = ["info1", "info2", "info3", "done"];
 
 const publish = () => {
   setTimeout(
-    () => infos.forEach((info) => pubsub.publish(TOPIC, { info })),
+    () => infos.forEach((info) => pubsub.publish(TOPIC, { info: info })),
     1000
   );
 };
 
+const returnState = () => {
+  setTimeout(
+      () => infos.forEach((info) => pubsub.publish(SECOND_TOPIC, { stat: info })),
+      1000
+  );
+}
+
 module.exports = {
   Query: {
-    go: () => {
+    go() {
       publish();
-
       return "going";
     },
+    doSomething() {
+      returnState();
+      return "doing";
+    }
   },
 
   Subscription: {
+    stat: {
+      subscribe: () => pubsub.asyncIterator([SECOND_TOPIC]),
+    },
     info: {
       subscribe: () => pubsub.asyncIterator([TOPIC]),
     },
+
   },
 };
